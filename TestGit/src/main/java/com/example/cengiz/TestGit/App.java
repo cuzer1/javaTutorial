292e34fa524f5875d373927a93ef472bf8c7975e
aa <cuzerms@yahoo.com>
2017-10-24 19:27:28 -0700

    Initial checkin

---
+++ TestGit/src/main/java/com/example/cengiz/TestGit/App.java
@@ -0,0 +1,13 @@
+package com.example.cengiz.TestGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
