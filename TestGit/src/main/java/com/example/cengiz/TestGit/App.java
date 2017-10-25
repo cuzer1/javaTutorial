7961fb93475a4e3554235776dc5226ca83c581e8
aa <cuzerms@yahoo.com>
2017-10-24 19:42:11 -0700

    fix another change

--- TestGit/src/main/java/com/example/cengiz/TestGit/App.java
+++ TestGit/src/main/java/com/example/cengiz/TestGit/App.java
@@ -8,5 +8,6 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Merhaba!!");
+		System.out.println("Added another output");
 	}
 }
