5348e47a389f0978b4e129afeeea7e1264d49c54
aa <cuzerms@yahoo.com>
2017-10-24 19:32:55 -0700

    Added sysout

--- TestGit/src/main/java/com/example/cengiz/TestGit/App.java
+++ TestGit/src/main/java/com/example/cengiz/TestGit/App.java
@@ -4,10 +4,9 @@ package com.example.cengiz.TestGit;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Merhaba!!");
+	}
 }
