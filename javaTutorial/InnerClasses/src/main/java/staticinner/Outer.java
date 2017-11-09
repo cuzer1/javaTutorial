c5630416d9a1a15545c8a42c079502c99daef9be
aa <cuzerms@yahoo.com>
2017-11-09 00:23:44 -0700

    S21 L167

--- javaTutorial/InnerClasses/src/main/java/staticinner/Outer.java
+++ javaTutorial/InnerClasses/src/main/java/staticinner/Outer.java
@@ -10,12 +10,20 @@ public class Outer {
 		static void f2() {
 			System.out.println("Inner Static Method");
 		}
+
+		void f3() {
+			System.out.println("Non Static Method inside the Inner Class");
+		}
 	}
 
 	public static void main(String[] args) {
 
 		Outer.f1();
 		Outer.Inner.f2();
+
+		Outer.Inner inner = new Outer.Inner();
+
+		inner.f3();
 
 	}
 
