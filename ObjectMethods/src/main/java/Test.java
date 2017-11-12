628fd9e9e18e834edc02fdc7e178215313ac46f7
aa <cuzerms@yahoo.com>
2017-11-12 11:10:45 -0700

    new project

---
+++ ObjectMethods/src/main/java/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Passenger passenger = new Passenger();
+		passenger.setFirstName("Cengiz");
+		passenger.setLastName("Uzer");
+		System.out.println(passenger);
+	}
+
+}
