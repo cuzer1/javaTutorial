e584b716f5bef4b7ad12ee0869eadbaab82af2aa
aa <cuzerms@yahoo.com>
2017-11-25 16:32:31 -0700

    Write to Json

---
+++ Examples/src/main/java/com/cengiz/examples/ReadFromFileWritetoJSONFile.java
@@ -0,0 +1,63 @@
+package com.cengiz.examples;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonEncoding;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+
+public class ReadFromFileWritetoJSONFile {
+
+	public static void main(String[] args) {
+
+		JsonFactory factory = new JsonFactory();
+
+		try (FileReader fr = new FileReader("input/dummyData.csv"); BufferedReader br = new BufferedReader(fr)) {
+
+			// Skip header
+			String headerLine = br.readLine();
+			String[] hdr = headerLine.split(",");
+
+			String line;
+
+			try {
+				JsonGenerator generator = factory.createGenerator(new File("output/dummyOutput.json"),
+						JsonEncoding.UTF8);
+
+				generator.writeStartArray();
+				while ((line = br.readLine()) != null) {
+
+					String[] l = line.split(",");
+
+					generator.writeStartObject();
+					generator.writeStringField(hdr[0], l[0]);
+					generator.writeStringField(hdr[1], l[1]);
+					generator.writeStringField(hdr[2], l[2]);
+					generator.writeStringField(hdr[3], l[3]);
+					generator.writeEndObject();
+
+				}
+				generator.writeEndArray();
+				generator.close();
+				System.out.println("File created...");
+
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		;
+	}
+
+}
--- Examples/src/main/java/com/cengiz/examples/Record.java
+++ Examples/src/main/java/com/cengiz/examples/Record.java
@@ -6,13 +6,13 @@ public class Record {
 	private String name;
 	private String email;
 	private String date;
-	
+
 	public Record(Long id, String name, String email, String date) {
 		this.Id = id;
 		this.name = name;
 		this.email = email;
 		this.date = date;
-		
+
 	}
 
 	public Long getId() {
@@ -51,7 +51,4 @@ public class Record {
 	public String toString() {
 		return "Record [Id=" + Id + ", name=" + name + ", email=" + email + ", date=" + date + "]";
 	}
-	
-	
-
 }
